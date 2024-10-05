2feb91c308ed97c7ca871b3d06b53dbd8de9c958
Sai-506 <satyasaikrishna299@gmail.com>
2024-10-06 00:11:31 +0530

    Update Cart.jsx

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-// import BuyNow from './BuyNow'
+//import BuyNow from './BuyNow'
 
 const Cart = ({cart,setCart}) => {
   return (
@@ -9,20 +9,27 @@ const Cart = ({cart,setCart}) => {
       {
         cart.length==0 ?(
 <>
+
 <div class="text-center" style={{width:"300px",
   height:"500px",
   position:"absolute",
- top:"100px",
+  
+  
+  
+  top:"100px",
   left:"500px",
   borderradius:"10px",
-  textalign: "center"}}>
-  <h1 style={{color:"red",width:"350px",}}>Place Your Order Here</h1>
+  textAlign: "center"
+  }}>
+    
+    <h1 style={{color:"red",width:"350px",}}>Place Your Order Here</h1> 
+  
   <form>
    
-    <label>Full Name:</label>
+     <label>Full Name:</label>
     <input type="text" value=""></input><br></br><br></br>
     <label>Mobile Number</label>
-    <input type="numbers" value=""></input><br></br><br></br><br></br>
+    <input type="numbers" value=""></input><br></br><br></br>
     <label>choose state</label>
     <select id="state" name="state">
     <option value="AndhraPradesh">AndhraPradesh</option>
@@ -40,15 +47,14 @@ const Cart = ({cart,setCart}) => {
     <label>LandMark</label>
     <input type="text" value=""></input><br></br><br></br>
     <div class="payment">
-    <a href="https://www.phonepe.com/">Payment Here</a><br></br>
-  </div>
+    <a href="https://www.phonepe.com/">Payment Here</a>
+    </div>
+  
    
   </form>
+  
     <Link to={"/"} className='btn btn-warning'>Continue Shopping...</Link>  
    
-  
-  
-  
   
   
   </div>  
@@ -57,7 +63,7 @@ const Cart = ({cart,setCart}) => {
       cart.map((product)=>{
         return(
           <>
-          <div className="card mb-3 my-5" style={{width:'700px'}}>
+          <div className="card" style={{width:'500px'}}>
   <div className="row g-0">
     <div className="col-md-4">
       <img src={product.imgSrc} className="img-fluid rounded-start" alt="..." />
